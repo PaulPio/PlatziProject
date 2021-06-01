31da98dfcb36171434b4d8f059389aa18ed2290b
PaulPio <paulrpiotrowski@gmail.com>
2021-06-01 14:40:31 -0400

    Arreglos en el proyecto de canvas

--- Curso de Prog Basica/canvas/eventos.js
+++ Curso de Prog Basica/canvas/eventos.js
@@ -5,46 +5,44 @@ var teclas = {
   RIGHT: 39
 };
 
-document.addEventListener("keydown", dibujarTeclado);
-var cuadrito = document.getElementById("area_de_dibujo");
-var papel = cuadrito.getContext("2d");
-var x = 150;
-var y = 150;
+var draw = document.getElementById("canvas");
+var ancho = document.getElementById("ancho_linea");
+var papel = draw.getContext("2d");
+var xf, yf, xi, yi;
+var colorcito = document.getElementById("color");
+var pintar = false;
+draw.addEventListener("mousedown", dibujando);
+function dibujando(evento) {
+  pintar = true;
+  xi = evento.offsetX;
+  yi = evento.offsetY;
+  console.log(evento);
+  draw.addEventListener("mousemove",function(evento_2) {
+    if (pintar && "onmousedown") {
+      xf = evento_2.offsetX;
+      yf = evento_2.offsetY;
+      dibujarlinea(colorcito, xi, yi, xf, yf, papel);
+      xi = xf;
+      yi = yf;
+      draw.addEventListener("mouseup", apagando);
+      draw.addEventListener("mouseout", apagando);
+      function apagando () {
+        console.log("Gola")
+        pintar = false;
+}
+}
+}, false);
+}
+// funcion para dibujar en el canvas
 
-dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
-
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+function dibujarlinea(color, xi, yi, xf, yf, lienzo)
 {
   lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.lineWidth = 3;
-  lienzo.moveTo(xinicial, yinicial);
-  lienzo.lineTo(xfinal, yfinal);
+  lienzo.strokeStyle = color.value;
+  lienzo.lineWidth = ancho.value;
+  lienzo.lineJoin = "round";
+  lienzo.moveTo(xi, yi);
+  lienzo.lineTo(xf, yf);
   lienzo.stroke();
   lienzo.closePath();
-}
-
-function dibujarTeclado(evento)
-{
-  var colorcito = "#FAA";
-  var movimiento = 5;
-  switch(evento.keyCode)
-  {
-    case teclas.UP:
-      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
-      y = y - movimiento;
-    break;
-    case teclas.DOWN:
-      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
-      y = y + movimiento;
-    break;
-    case teclas.LEFT:
-      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
-      x = x - movimiento;
-    break;
-    case teclas.RIGHT:
-      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
-      x = x + movimiento;
-    break;
-  }
 }
