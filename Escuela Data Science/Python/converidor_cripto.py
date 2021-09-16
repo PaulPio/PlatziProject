43a70bc3eea30d2571727924d3b5d748d02c20a8
PaulPio <paulrpiotrowski@gmail.com>
2021-09-16 19:00:33 -0400

    Cambios en los convertidores

--- Escuela Data Science/Python/converidor_cripto.py
+++ Escuela Data Science/Python/converidor_cripto.py
@@ -2,14 +2,23 @@
 #Se usa la libreria de coinmarketcapapi 
 from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
 
-cmc = CoinMarketCapAPI('a51e0316-a6e1-49f6-a412-1417d444d1a9') #Api clave
+def run():
+    """ To run the program"""
+    
+    cmc = CoinMarketCapAPI('a51e0316-a6e1-49f6-a412-1417d444d1a9') #Api clave
 
-#r = cmc.cryptocurrency_info(symbol='BTC')  #Para obtener info acerca del symbolo
+    #r = cmc.cryptocurrency_info(symbol='BTC')  #Para obtener info acerca del symbolo
 
-buscar = input("Coloque el simbolo de la cripto que desee encontrar: \n")
+    buscar = input("Coloque el simbolo de la cripto que desee encontrar: \n")
 
-try:
-    data_quote = cmc.cryptocurrency_quotes_latest(symbol=buscar, convert='USD') #Para obtener el precio de una cripto
-    print(data_quote.data[buscar]['quote']['USD']['price']) #Para imprimir el precio buscando en la api
-except Exception:
-    print("Hubo un problema")
+    try:
+        data_quote = cmc.cryptocurrency_quotes_latest(symbol=buscar, convert='USD') #Para obtener el precio de una cripto
+        print(data_quote.data[buscar]['data']['name'] + "\n") #Para imprimir el precio buscando en la api
+        print(data_quote.data[buscar]['quote']['USD']['price']) #Para imprimir el precio buscando en la api
+    except Exception:
+        print("Hubo un problema")
+
+
+
+if __name__ == "__main__":
+    run()
--- Escuela Data Science/Python/currency_exchange.py
+++ Escuela Data Science/Python/currency_exchange.py
@@ -1,22 +1,31 @@
 #Programa para convertir monedas usando la api de exchanges rates api
 #Program to covert currency using exchanges rates api
 import requests
-APIKEY = "d9613ad81bb7f07a9efe9535" #API CODE
 
-# Welcome Message
-print("Welcome to the currency convertor program.\n") 
+def run():
+    """ To run the program"""
+    APIKEY = "d9613ad81bb7f07a9efe9535" #API CODE, you need an apikey to use it, 
+    # it's free, you just have to create an account in https://www.exchangerate-api.com/ to get a key
 
-#Introducing the tickets of the currencys
-dato1= input("Introduce the first ticket currency to compare: \n")
+    # Welcome Message
+    print("Welcome to the currency convertor program.\n") 
 
-dato2= input("Introduce the second ticket currency to compare: \n")
+    #Introducing the tickets of the currencys
+    dato1= input("Introduce the first ticket currency to compare: \n")
 
-#url adding th apikey and the fist ticket
-url = "https://v6.exchangerate-api.com/v6/" + APIKEY + "/latest/" + dato1
+    dato2= input("Introduce the second ticket currency to compare: \n")
 
-#accesing the url
-info = requests.get(url)
-#translating the json to a dictionary
-data = info.json()
-#Printing the conversion rate of the second ticket
-print(data['conversion_rates'][dato2])
+    #url adding th apikey and the fist ticket
+    url = "https://v6.exchangerate-api.com/v6/" + APIKEY + "/latest/" + dato1
+
+    #accesing the url
+    info = requests.get(url)
+    #translating the json to a dictionary
+    data = info.json()
+    #Printing the conversion rate of the second ticket
+    print(f"El precio del {dato1} en {dato2} es de: " + str(data['conversion_rates'][dato2]))
+
+
+
+if __name__ == "__main__":
+    run()
